34b987822223237a0ed3cabacfa87f05ea7c6730
Jasurbek Kalandarov <jkalandarov@gmail.com>
2023-05-12 22:31:29 +0500

    updated ExpenseItem with Card

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,6 +1,7 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import { useState } from 'react';
+import Card from './Card';
 
 function ExpenseItem(props) {
   const [ initialName, setName ] = useState(props.name);
@@ -9,14 +10,14 @@ function ExpenseItem(props) {
   }
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{initialName}</h2>
       </div>
       <div className="expense-item__price">${props.amount}</div>
       <button onClick={handleClick}>Edit details</button>
-    </div>
+    </Card>
   )
 }
 
