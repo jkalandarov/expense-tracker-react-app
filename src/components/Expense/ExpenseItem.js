b1cf4cf3ea7b14a36407b53c6e0d3ece36f84d08
Jasurbek Kalandarov <jkalandarov@gmail.com>
2023-05-12 22:26:30 +0500

    added useState

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,14 +1,21 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
+import { useState } from 'react';
 
 function ExpenseItem(props) {
+  const [ initialName, setName ] = useState(props.name);
+  const handleClick = () => {
+    setName('Updated details');
+  }
+
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.name}</h2>
+        <h2>{initialName}</h2>
       </div>
       <div className="expense-item__price">${props.amount}</div>
+      <button onClick={handleClick}>Edit details</button>
     </div>
   )
 }
