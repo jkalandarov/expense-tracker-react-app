146fa30833bc87202e606f3e5dfd9fd4d5ac909e
Jasurbek Kalandarov <jkalandarov@gmail.com>
2023-05-14 21:40:59 +0500

    updated initial list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Expenses from "./components/Expense/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 const expenses = [
   {
-    date: { month: 'May', day: 1, year: 2023 }, name: 'Car insurance', amount: 295.75
+    id: 'e1',
+    name: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', name: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    name: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
   },
   {
-    date: { month: 'May', day: 2, year: 2023 }, name: 'Groceries', amount: 92.16
+    id: 'e4',
+    name: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
   },
 ];
 
 function App() {
+  const [ intialExpenses, setExpenses ] = useState(expenses);
   const handleAddExpense = expense => {
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   }
   return (
     <div className="App">
       <h2>Expense Tracker</h2>
       <NewExpense onAddExpense={handleAddExpense}/>
-      <Expenses items={expenses} />
+      <Expenses items={intialExpenses} />
     </div>
   );
 }
