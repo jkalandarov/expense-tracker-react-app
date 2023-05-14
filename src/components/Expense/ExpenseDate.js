bfd029219e603fe574ad1f2b61229e1003d198a9
Jasurbek Kalandarov <jkalandarov@gmail.com>
2023-05-14 21:41:31 +0500

    refactores ExpenseDate

--- src/components/Expense/ExpenseDate.js
+++ src/components/Expense/ExpenseDate.js
@@ -1,11 +1,16 @@
 import './ExpenseDate.css';
 
 function ExpenseDate(props) {
+  const date = new Date(props.date);
+  const month = date.toLocaleString('en-US', { month: 'long' });
+  const day = date.toLocaleString('en-US', { day: '2-digit' });
+  const year = date.getFullYear();
+
   return (
     <div className='expense-date'>
-      <div className='expense-date__month'>{props.date.month}</div>
-      <div className='expense-date__day'>{props.date.day}</div>
-      <div className='expense-date__year'>{props.date.year}</div>
+      <div className='expense-date__month'>{month}</div>
+      <div className='expense-date__day'>{day}</div>
+      <div className='expense-date__year'>{year}</div>
     </div>
   )
 }
