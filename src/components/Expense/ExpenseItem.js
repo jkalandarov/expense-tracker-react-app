3df27579bb773cfe2c8c36848b4910a56587179b
Jasurbek Kalandarov <jkalandarov@gmail.com>
2023-05-14 21:42:02 +0500

    removed unnecessary useSate

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,22 +1,15 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
-import { useState } from 'react';
 import Card from './Card';
 
 function ExpenseItem(props) {
-  const [ initialName, setName ] = useState(props.name);
-  const handleClick = () => {
-    setName('Updated details');
-  }
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{initialName}</h2>
+        <h2>{props.name}</h2>
       </div>
       <div className="expense-item__price">${props.amount}</div>
-      <button onClick={handleClick}>Edit details</button>
     </Card>
   )
 }
