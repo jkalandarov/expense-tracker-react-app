c0d27c9171579e050639e9535442fc4868306730
Jasurbek Kalandarov <jkalandarov@gmail.com>
2023-05-15 22:08:26 +0500

    fixed years

--- src/App.js
+++ src/App.js
@@ -7,20 +7,20 @@ const expenses = [
     id: 'e1',
     name: 'Toilet Paper',
     amount: 94.12,
-    date: new Date(2020, 7, 14),
+    date: new Date(2021, 7, 14),
   },
   { id: 'e2', name: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
   {
     id: 'e3',
     name: 'Car Insurance',
     amount: 294.67,
-    date: new Date(2021, 2, 28),
+    date: new Date(2022, 2, 28),
   },
   {
     id: 'e4',
     name: 'New Desk (Wooden)',
     amount: 450,
-    date: new Date(2021, 5, 12),
+    date: new Date(2023, 5, 12),
   },
 ];
 
